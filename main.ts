5d1b3643ab0f45291eb87696b36a14aea7fd9bdc
default <ketia.ineza@stu.ocsb.ca>
2025-11-26 21:06:10 +0000

    Calculated the average of a number

---
+++ main.ts
@@ -0,0 +1,7 @@
+const num1 = 56.9;
+const num2 = 89.7;
+const num3 = 90.2;
+
+const average = (num1 + num2 + num3) / 3;
+
+console.log("The average of " + num1 + ", " + num2 + ", and " + num3 + " is " + average.toFixed(2) + ".");
